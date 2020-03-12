7a2ba4c2a80bf645700824e18f58156cf86be15c
nikitaiavdeev <49996429+nikitaiavdeev@users.noreply.github.com>
2020-03-12 17:07:40 -0400

    Add files via upload

--- components/PanelInput.vue
+++ components/PanelInput.vue
@@ -5,9 +5,22 @@
     <v-card-text>
       <v-row>
         <v-col>
-          <svg id="canvas" width="100%" height="400px">
-            <g v-for="(beam, index) in beams" :key="index"  class='beam'>
-              <path  :d="getBeamRect(index)" fill="none" stroke-width="3px"/>
+          <svg xmlns="http://www.w3.org/2000/svg" id="canvas" width="100%" height="400px">
+            <defs>
+              <marker id="arrow" viewBox="0 0 10 10" refX="10" refY="5" markerWidth="10" markerHeight="10" orient="auto-start-reverse">
+                <path d="M0,0L10,5L0,10z" stroke="none" fill="context-stroke"/>
+              </marker>
+            </defs>
+
+            <g v-for="(section, index) in beams.sections" :key="'beam' + index"  class='beam'>
+              <polygon  :points="section.polygonWhite" fill="white"/>
+              <polygon  :points="section.polygonFill"/>
+              <path  :d="section.path"/>
+            </g>
+
+            <g v-for="(item, index) in forces.distributed" :key="'distrForce' + index"  class='distrForce'>
+              <path  :points="item.path + 'z'"/>
+              <path  :points="item.path" marker-start="url(#arrow)" marker-end="url(#arrow)"/>
             </g>
           </svg>
         </v-col>
@@ -17,14 +30,15 @@
 </template>
 
 <script>
-  import { mapState, mapGetters  } from 'vuex'
+  import { mapState, mapMutations  } from 'vuex'
   import mapStatesTwoWay from '../store/mapTwoWay'
+  import { MARGIN_X, MARGIN_Y } from '../store/store.js'
   import '../scss/svg.scss'
 
   export default {
     computed:{
-      ...mapState(['beams']),
-      ...mapGetters(['getBeamRect']),
+      ...mapState(['beams', 'forces']),
+      ...mapMutations(['updateBeamsSVG', 'updateForcesSVG']),
       ...mapStatesTwoWay({
         screen: state => state.screen,
       }, function (value) {
@@ -36,8 +50,13 @@
     }),
 
     mounted: function() {
-      window.addEventListener('resize', this.onResize);
-      this.screen.MAX_X = document.getElementById('canvas').clientWidth;
+      this.$nextTick(() => {
+        window.addEventListener('resize', this.onResize);
+        this.screen.maxX = document.getElementById('canvas').clientWidth - 2 * MARGIN_X;
+        this.screen.maxY = document.getElementById('canvas').clientHeight- 2 * MARGIN_Y;
+        this.updateBeamsSVG;
+        this.updateForcesSVG;
+      })
     },
 
     beforeDestroy: function() { 
@@ -46,7 +65,10 @@
 
     methods: {
       onResize() {
-        this.screen.MAX_X = document.getElementById('canvas').clientWidth;
+        this.screen.maxX = document.getElementById('canvas').clientWidth - 2 * MARGIN_X;
+        this.screen.maxY = document.getElementById('canvas').clientHeight- 2 * MARGIN_Y;
+        this.updateBeamsSVG;
+        this.updateForcesSVG;
       }
     }
   }
--- store/store.js
+++ store/store.js
@@ -3,67 +3,150 @@ import Vue from 'vue'
 
 Vue.use(Vuex)
 
+export const
+  MARGIN_X = 10,
+  MARGIN_Y = 10;
+
+const
+  MIN_BEAM_HEIGHT = 0.02,
+  MAX_BEAM_HEIGHT = 0.05,
+  MIN_FORCE_HEIGHT = 0.03,
+  MAX_FORCE_HEIGHT = 0.30;
+
+
 const defaultState = {
-    screen:{
-      MIN_X: 10,
-      MAX_X: 0,
-      MIN_Y: 10,
-      MAX_Y: 30,
-    },
-    beams:{ 
-      totalLength: 30,
-      maxHeight: 1,
-      minHeight: Math.sqrt(0.5),
-      sections: [ {
-          length: 10,
-          areaA: 1.0,
-          areaB: 0.5,
-          inerA: 1.0,
-          inerB: 1.0,
-        }, {
-          length: 20,
-          areaA: 0.8,
-          areaB: 0.7,
-          inerA: 1.0,
-          inerB: 1.0,
-        }],
-    },
-    appVersion: 'v0.0.1',
-    snackbar: {
-      message: '',
-      color: '',
-      timeout: 5000,
-      show: false
-    }
-  };
+  screen: {
+    maxX: 0,
+    maxY: 0,
+    scaleX: 0,
+    beamY: 0,
+  },
+  beams: {
+    totalLength: 30,
+    maxHeight: 1,
+    minHeight: 1,
+    sections: [{
+      length: 10,
+      areaA: 1.0,
+      areaB: 1,
+      inerA: 1.0,
+      inerB: 1.0,
+      path: '',
+      polygonFill: '',
+      polygonWhite: '',
+    }, {
+      length: 20,
+      areaA: 1,
+      areaB: 1,
+      inerA: 1.0,
+      inerB: 1.0,
+      path: '',
+      polygonFill: '',
+      polygonWhite: '',
+    }],
+  },
+  forces: {
+    maxForce: 100,
+    minForce: 10,
+    distributed: [{
+      locA: 2,
+      locB: 4,
+      valA: 100,
+      valB: 50,
+      path: ''
+    }, {
+      locA: 2,
+      locB: 4,
+      valA: -90,
+      valB: -60,
+      path: ''
+    }]
+  },
+  appVersion: 'v0.0.1',
+  snackbar: {
+    message: '',
+    color: '',
+    timeout: 5000,
+    show: false
+  }
+};
 
 
 export default new Vuex.Store({
   state: Object.assign({}, defaultState),
-  
-  getters: {
-    getBeamRect: state => (id) => {
-      const len = state.beams[id].length,
-            heightA = Math.sqrt(state.beams[id].areaA * Math.pow(state.beams[id].inerA, 1/3)),
-            heightB = Math.sqrt(state.beams[id].areaB * Math.pow(state.beams[id].inerB, 1/3)),
-            xScale = (state.BEAM_MAX_X-2*BEAM_MIN_X)/state.totalLength,
-            yScale = Number.isFinite((BEAM_MAX_Y-BEAM_MIN_Y)/(state.minAreaIner-state.maxAreaIner)) ? (BEAM_MAX_Y-BEAM_MIN_Y)/(state.minAreaIner-state.maxAreaIner) : 0.0,
-            getH = (h) => {return BEAM_MAX_Y - yScale * (h-state.maxAreaIner)};
-      let x0=0;
-      for(let i=0; i < id; i++) x0+=state.beams[i].length;
-      return 'M' + (BEAM_MIN_X+x0*xScale) + ' 0 h'+ len*xScale + 'v' + getH(hB) + 'L' + (BEAM_MIN_X+x0*xScale) + ' ' + getH(hA) +'Z'
-    }
-  },
-  
+
+  getters: {},
+
   mutations: {
-    resetState(state){
+    resetState(state) {
       Object.assign(state, defaultState);
     },
     updateCurrent(state, payload) {
       Object.assign(state, payload);
     },
-    showMessage (state, payload) {
+    showMessage(state, payload) {
       Object.assign(state.snackbar, payload);
+    },
+    updateBeamsSVG: state => {
+      const
+        beams = state.beams,
+        sections = beams.sections,
+        screen = state.screen,
+        ySlope = (MAX_BEAM_HEIGHT - MIN_BEAM_HEIGHT) / (beams.minHeight - beams.maxHeight),
+        yScale = Number.isFinite(ySlope) ? ySlope : 0.0;
+
+      const
+        getX = (x) => {
+          return MARGIN_X + x * screen.scaleX;
+        },
+        getY = (y) => {
+          return MARGIN_Y + screen.maxY * 0.5 * (1 + Math.sign(y) * (MAX_BEAM_HEIGHT - yScale * (Math.abs(y) - beams.maxHeight)));
+        },
+        getPoint = (x, y) => {
+          return getX(x) + ',' + getY(y);
+        }
+      
+      let x0 = 0,
+        x1 = 0,
+        length, heightA, heightB;
+      
+      //set screen constants
+      screen.scaleX = screen.maxX / beams.totalLength;
+      screen.beamY = getY(beams.maxHeight) + 2;
+
+      sections.forEach(section => {
+        length = section.length;
+        heightA = 0.5 * (section.areaA + Math.pow(section.inerA, 1 / 3));
+        heightB = 0.5 * (section.areaB + Math.pow(section.inerB, 1 / 3));
+        x0 = x1;
+        x1 += length;
+        section.polygonWhite = getPoint(x0, -beams.maxHeight) + ' ' + getPoint(x1, -beams.maxHeight) + ' ' + getPoint(x1, beams.maxHeight) + ' ' + getPoint(x0, beams.maxHeight);
+        section.polygonFill = getPoint(x0, -heightA) + ' ' + getPoint(x1, -heightB) + ' ' + getPoint(x1, heightB) + ' ' + getPoint(x0, heightA);
+        section.path = 'M' + getPoint(x0, -beams.maxHeight) + 'L' + getPoint(x1, -beams.maxHeight) + 'M' + getPoint(x0, beams.maxHeight) + 'L' + getPoint(x1, beams.maxHeight);
+      });
+    },
+    updateForcesSVG: state => {
+      const
+        forces = state.forces,
+        screen = state.screen,
+        distributed = forces.distributed,
+        ySlope = (MAX_FORCE_HEIGHT - MIN_FORCE_HEIGHT) / (forces.minForce - forces.maxForce),
+        yScale = Number.isFinite(ySlope) ? ySlope : 0.0;
+
+      const
+        getX = (x) => {
+          return MARGIN_X + x * screen.scaleX;
+        },
+        getY = (y) => {
+          return MARGIN_Y + screen.maxY * 0.5 * (1 + Math.sign(y) * (MAX_FORCE_HEIGHT - yScale * (Math.abs(y) - forces.maxForce)));
+        },
+        getPoint = (x, y) => {
+          return getX(x) + ',' + getY(y);
+        };
+
+      distributed.forEach(f => {
+        f.path = 'M' + getX(f.locA) + ',' + Math.sign(f.valA) * screen.beamY + 'L' + getPoint(f.locA, f.valA) + 'L' + getPoint(f.locB, f.valB) + 'L' + getX(f.locB) + ',' + Math.sign(f.valB) * screen.beamY;
+      });
     }
   },
 });
